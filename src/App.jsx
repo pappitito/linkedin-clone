fc38ca31d6eb837a5c21c5c393021d0fcfcef31a
Tito <107757681+pappitito@users.noreply.github.com>
2023-05-30 10:47:33 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
 import Header from './components/Header/Header';
@@ -6,22 +6,48 @@ import Sidebar from './components/LeftSide/Sidebar';
 import MiddleBar from './components/MiddleBar/MiddleBar';
 import AddFeed from './components/RightSide/AddFeed';
 import { useSelector } from 'react-redux';
-import { selectUser } from './app/userSlice';
+import { accountAvailable, logout, login, selectUser } from './app/userSlice';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
 
 function App() {
 
   const user = useSelector(selectUser)
+  const accountAvail = useSelector(accountAvailable)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (userCredentials)=>{
+      if(userCredentials){
+        dispatch(login({
+          email: userCredentials.email,
+          uid: userCredentials.uid,
+          name: userCredentials.displayName,
+          avatar: userCredentials.photoURL
+        }))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return (
-    <div className="App">
-      {!user? <Login/> : <div>
+    <div className="App relative">
+      {!user? accountAvail? <Login/> : <SignUp /> : <div>
         <Header />
-        <div className='flex flex-row justify-center w-[100%] gap-5 p-4'>
+        
+        <div className='flex flex-row sticky  justify-center w-[100%] gap-5 p-4'>
             <Sidebar/>
             <MiddleBar />
             <AddFeed />
+            
           </div>
+          
       
       </div> }
     </div>
