3c6a45b676fab75cde89d06bf92647399a1af165
Tito <107757681+pappitito@users.noreply.github.com>
2023-06-01 13:09:11 +0100

    i've tried

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Sidebar from './components/LeftSide/Sidebar';
 import MiddleBar from './components/MiddleBar/MiddleBar';
 import AddFeed from './components/RightSide/AddFeed';
 import { useSelector } from 'react-redux';
-import { accountAvailable, logout, login, selectUser } from './app/userSlice';
+import { accountAvailable, logout, login, selectUser, isSearchActive, toggleSearchActive, toggleMobileActive, toggleMobileInactive } from './app/userSlice';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -17,6 +17,7 @@ function App() {
 
   const user = useSelector(selectUser)
   const accountAvail = useSelector(accountAvailable)
+  const searchActive = useSelector(isSearchActive)
 
   const dispatch = useDispatch()
 
@@ -29,6 +30,7 @@ function App() {
           name: userCredentials.displayName,
           avatar: userCredentials.photoURL
         }))
+        
       }
       else {
         dispatch(logout())
@@ -40,11 +42,16 @@ function App() {
     <div className="App relative">
       {!user? accountAvail? <Login/> : <SignUp /> : <div>
         <Header />
-        
-        <div className='flex flex-row sticky  justify-center w-[100%] gap-5 p-4'>
-            <Sidebar/>
-            <MiddleBar />
-            <AddFeed />
+        {searchActive && <div onClick={()=>{
+          dispatch(toggleMobileInactive())
+          dispatch(toggleSearchActive())
+          }} className='absolute z-[200] w-[100vw] h-[100vh] bg-black opacity-30'></div>}
+        <div className='flex flex-col md:flex-row   justify-center w-[100%] gap-5 p-4'>
+            <div onClick={()=>test()}><Sidebar/></div>
+            <div className='flex flex-col gap-5 lg:flex-row'>
+                <MiddleBar />
+                <div><AddFeed /></div>
+            </div>
             
           </div>
           
